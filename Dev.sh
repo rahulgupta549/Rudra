af1ba7a2f4524f1a605b2be49009bb6603549180
rahulgupta549 <49826993+rahulgupta549@users.noreply.github.com>
2019-06-09 13:23:27 +0530

    Create Dev.sh

---
+++ Dev.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function1() {
+
+                df -h
+                ifconfig -a
+}
+
+
+echo  "this is my 1st script"
+
+read -p " enter the value of A: " var1
+
+read -p " enter the option [Y/N] " choicevalue
+
+if [ $var1 == 5 ]; then
+        echo " the input value is $var1 "
+
+else
+        echo " the value type is not equal to 5: $var1 "
+        exit 1
+fi
+
+if [ $choicevalue = "Y"  -o  $choicevalue = "y" ] ; then
+                function1
+         else
+                echo " your choice is 'NO' "
+                exit 2
+fi
